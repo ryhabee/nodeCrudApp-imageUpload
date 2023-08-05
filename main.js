1549026da78555be6ed737631b99ab54837a6e64
ryhabee <belarbirihab.contact@gmail.com>
2023-08-05 17:05:26 +0100

    setup

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
 // imports
 require('dotenv').config() ;
 const express =require('express' ) ;
-const mongoose =require('mongoose') ;
+const dbConfig = require('./dbConfig') ;
 const session=require('express-session') ; 
-
-
+ 
 const app = express() ;
 const PORT = process.env.PORT ||4000 ; 
 
-app.get('/' ,(req,res)=>{
-    res.send('hello world')
-})
+//middelwares
+app.use(express.urlencoded({extended : false}))
+app.use(express.json()) ;
+
+app.use(session({
+    secret: 'keyboard cat',
+    saveUninitialized : true ,
+    resave :false ,
+})) ;
+app.use((req, res, next)=>{
+    res.locals.message=req.session.message ;
+    delete req.session.message ; 
+    next() ; 
+  }
+);
+//set template engine
+app.set('view engine', 'ejs') ; 
+
+// routes prefix
+app.use('/' ,require('./routes/routes')) ;
 
 app.listen(PORT ,()=>{
     console.log(`Server is running on port ${PORT}`)
